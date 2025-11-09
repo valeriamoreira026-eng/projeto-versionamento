e1ab0a3ec4b44c7ec8a18b02babf1fbdcfb8ea2f
valeriamoreira026-eng <valeria.moreira026@gmail.com>
2025-11-09 15:46:02 -0300

    feat(login): criação inicial da classe de login

---
+++ src/LoginFeature.java
@@ -0,0 +1,9 @@
+public class LoginFeature {
+    public static void main(String[] args) {
+        login();
+    }
+
+    public static void login() {
+        System.out.println("Simulando login...");
+    }
+}
